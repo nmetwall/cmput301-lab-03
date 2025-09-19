86099040465900831c8df65b6d50bead20759108
fairytale twists <nmetwall@ualberta.ca>
2025-09-19 15:57:55 -0600

    not showing previous city/province

---
+++ code/app/src/main/java/com/example/listycitylab3/AddCityFragment.java
@@ -0,0 +1,57 @@
+package com.example.listycitylab3;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class AddCityFragment extends DialogFragment {
+
+    interface AddCityDialogListener {
+        void addCity(City city);
+    }
+
+    private AddCityDialogListener addListener;
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof AddCityDialogListener) {
+            addListener = (AddCityDialogListener) context;
+        }
+        else {
+            throw new RuntimeException(context +
+                    "must implement AddCityDialogueListener");
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_add_city, null);
+        EditText editCityName = view.findViewById(R.id.edit_text_city_text);
+        EditText editProvinceName = view.findViewById(R.id.edit_text_province_text);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+
+
+        return builder
+                        .setView(view)
+                        .setTitle("Add a city")
+                        .setNegativeButton("Cancel", null)
+                        .setPositiveButton("Add", (dialog, which) -> {
+                            String cityName = editCityName.getText().toString();
+                            String provinceName = editProvinceName.getText().toString();
+                            addListener.addCity(new City(cityName, provinceName));
+                        })
+                        .create();
+    }
+}
--- code/app/src/main/java/com/example/listycitylab3/MainActivity.java
+++ code/app/src/main/java/com/example/listycitylab3/MainActivity.java
@@ -1,36 +1,83 @@
 package com.example.listycitylab3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements AddCityFragment.AddCityDialogListener,
+        EditCityFragment.EditCityDialogListener {
 
-    private ArrayList<String> dataList;
+    private ArrayList<City> dataList;
     private ListView cityList;
-    private ArrayAdapter<String> cityAdapter;
+    private CityArrayAdapter cityAdapter;
+
+    // this is to add a city that is typed to the data set
+    // used in AddCityFragment.java
+    @Override
+    public void addCity(City city) {
+        cityAdapter.add(city);
+        cityAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void editCity(City city, String cityName, String provinceName) {
+        city.setName(cityName);
+        city.setProvince(provinceName);
+        cityAdapter.notifyDataSetChanged();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] cities = {
-                "Edmonton", "Vancouver", "Moscow",
-                "Sydney", "Berlin", "Vienna",
-                "Tokyo", "Beijing", "Osaka", "New Delhi"
-        };
+        // default city and province values
+        String[] cities = {"Edmonton", "Vancouver", "Montreal"};
+        String[] provinces = {"AB", "BC", "QC"};
 
-        dataList = new ArrayList<>();
-        dataList.addAll(Arrays.asList(cities));
-        
+        //adding the cities and provinces to dataList
+        dataList = new ArrayList<City>();
+        for (int i = 0; i < cities.length; i++) {
+            dataList.add(new City(cities[i], provinces[i]));
+        }
+
+        // city_list exists in activity_main.xml
+        // this is the list of cities/provinces that shows up under the header
+        // and before the the footer
         cityList = findViewById(R.id.city_list);
-        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        // converting the array of cities to be copied into the list of cities
+        cityAdapter = new CityArrayAdapter(this, dataList);
         cityList.setAdapter(cityAdapter);
+
+        // this is where the the floating action button (fab) connects to the AddCityFragment
+        // and therefore create the dialogue pop-up
+        FloatingActionButton fab = findViewById(R.id.button_add_city);
+        fab.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                new AddCityFragment().show(getSupportFragmentManager(), "Add City");
+            }
+        });
+
+        // repeat above for clicking on a city from the list
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long cityId) {
+                City city = (City) parent.getItemAtPosition(position);
+                EditCityFragment fragment = new EditCityFragment();
+                fragment.getCity(city);
+                fragment.show(getSupportFragmentManager(), "Edit City");
+            }
+        });
     }
 }
